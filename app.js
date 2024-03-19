36e58b2b78e8810afbc10993755e67acb83d97d7
Alina7121 <113956800+Alina7121@users.noreply.github.com>
2024-03-19 10:44:44 -0400

    Footer added

--- app.js
+++ app.js
@@ -31,3 +31,15 @@ function submitAnswer() {
     var selectedOption = document.querySelector('.option.selected').textContent;
     alert('You selected: ' + selectedOption);
 }
+//Function to keep the score of the players----------------------------------------------------------------------------------------------------------------
+let player1Score = 0;
+let player2Score = 0;
+
+
+// Example usage: Call this function to reset scores
+function resetScores() {
+    player1Score = 0;
+    player2Score = 0;
+    document.getElementById('player1').value = player1Score;
+    document.getElementById('player2').value = player2Score;
+}
