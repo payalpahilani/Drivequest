02d0bdd9d9c7f2e02c79498a76d88a1d70abe722
payalpahilani <58146637+payalpahilani@users.noreply.github.com>
2024-04-20 00:58:13 -0400

    changes

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const Player = require('./models/player.js');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public')); 
+
+app.get('/', (req, res) => {
+    res.send('Welcome to DriveQuest!');
+});
+
 
 // MongoDB connection
 mongoose.connect('mongodb+srv://payalp:drivequest2024@atlascluster.zrn4s0n.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster', {
