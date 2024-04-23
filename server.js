ad79d23aa63c4738f25fd3ce11dc9d49533445b2
payalpahilani <58146637+payalpahilani@users.noreply.github.com>
2024-04-23 14:26:37 -0400

    testing changes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
 const Question = require('./models/question.js');
 const Player = require('./models/player.js');
